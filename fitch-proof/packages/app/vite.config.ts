ffffae3f8e35b2bc120c367f12f5d33fe06e7175
niwla23 <46248939+niwla23@users.noreply.github.com>
2025-09-15 23:10:06 +0200

    fix latex export

--- fitch-proof/packages/app/vite.config.ts
+++ fitch-proof/packages/app/vite.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '',
+  build: {
+    rollupOptions: {
+      input: { main: "index.html", latex_export: "latex_export.html" }
+    }
+  }
 })
 
