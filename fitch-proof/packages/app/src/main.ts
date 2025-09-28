dbfa91aa2baced8a038d85eb611b4d93070399f4
niwla23 <46248939+niwla23@users.noreply.github.com>
2025-09-28 18:19:43 +0200

    improve tabbing

--- fitch-proof/packages/app/src/main.ts
+++ fitch-proof/packages/app/src/main.ts
@@ -150,8 +150,6 @@ editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, function() {
   format();
 });
 
-// VIBE CODE SECTION
-
 // Add custom key bindings
 editor.addCommand(monaco.KeyCode.Tab, function() {
   insertPipe(editor);
@@ -174,35 +172,22 @@ function insertPipe(editor: monaco.editor.IStandaloneCodeEditor) {
   // const model = editor.getModel();
 
   if (selection.isEmpty()) {
-    // Single cursor - insert pipe at cursor position
     const position = editor.getPosition();
+    const lineContent = model.getLineContent(position.lineNumber);
+
+    const column = lineContent.length - lineContent.split("").reverse().findIndex((v) => v == '|');
 
     editor.pushUndoStop();
     editor.executeEdits("insert-pipe", [{
       range: new monaco.Range(
         position.lineNumber,
-        position.column,
+        column - 1,
         position.lineNumber,
-        position.column,
+        column,
       ),
-      text: "| ",
+      text: " | ",
     }]);
-    editor.pushUndoStop();
-  } else {
-    // Multi-line selection - add pipe at start of each line
-    const startLine = selection.startLineNumber;
-    const endLine = selection.endLineNumber;
-    const edits = [];
-
-    for (let lineNumber = startLine; lineNumber <= endLine; lineNumber++) {
-      edits.push({
-        range: new monaco.Range(lineNumber, 1, lineNumber, 1),
-        text: "| ",
-      });
-    }
-
-    editor.pushUndoStop();
-    editor.executeEdits("insert-pipe-multiline", edits);
+    editor.setPosition(new monaco.Position(position.lineNumber, column + 4));
     editor.pushUndoStop();
   }
 }
@@ -212,44 +197,22 @@ function removePipe(editor: monaco.editor.IStandaloneCodeEditor) {
   const model = editor.getModel();
 
   if (selection.isEmpty()) {
-    // Single cursor - remove pipe before cursor if it exists
     const position = editor.getPosition();
     const lineContent = model.getLineContent(position.lineNumber);
 
-    if (
-      position.column > 1 && lineContent.charAt(position.column - 2) === "|"
-    ) {
+    const column = lineContent.length - lineContent.split("").reverse().findIndex((v) => v == '|');
+    if (column > 3) {
       editor.pushUndoStop();
       editor.executeEdits("remove-pipe", [{
         range: new monaco.Range(
           position.lineNumber,
-          position.column - 1,
+          column - 2,
           position.lineNumber,
-          position.column,
+          column,
         ),
         text: "",
       }]);
-      editor.pushUndoStop();
-    }
-  } else {
-    // Multi-line selection - remove pipe from start of each line
-    const startLine = selection.startLineNumber;
-    const endLine = selection.endLineNumber;
-    const edits = [];
-
-    for (let lineNumber = startLine; lineNumber <= endLine; lineNumber++) {
-      const lineContent = model.getLineContent(lineNumber);
-      if (lineContent.startsWith("|")) {
-        edits.push({
-          range: new monaco.Range(lineNumber, 1, lineNumber, 2),
-          text: "",
-        });
-      }
-    }
-
-    if (edits.length > 0) {
-      editor.pushUndoStop();
-      editor.executeEdits("remove-pipe-multiline", edits);
+      editor.setPosition(new monaco.Position(position.lineNumber, column));
       editor.pushUndoStop();
     }
   }
@@ -436,7 +399,6 @@ function format() {
     lineNumber: pos.lineNumber,
     column: editor.getModel().getLineMaxColumn(pos.lineNumber),
   });
-  // document.getElementById("proof-field").value = formatted;
   process_user_input();
 }
 
