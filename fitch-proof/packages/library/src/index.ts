868830ae4c82bdabf1c22fe1b18171532619cba0
niwla23 <46248939+niwla23@users.noreply.github.com>
2025-11-11 20:11:53 +0100

    restructure, move webui to its own root level directory

--- fitch-proof/packages/library/src/index.ts
+++
@@ -1,6 +0,0 @@
-import init, { check_proof, check_proof_with_template, format_proof, fix_line_numbers_in_proof, export_to_latex } from '../wasm/index';
-
-
-export {
-  init, check_proof, check_proof_with_template, format_proof, fix_line_numbers_in_proof, export_to_latex
-}
